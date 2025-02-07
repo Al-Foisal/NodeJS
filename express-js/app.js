3aeefd3431f9c296f344493de65384214bbe3887
Md Hafiz Al Foisal <alfoisal940700@gmail.com>
2025-02-07 18:59:25 +0600

    express: middleware base route approach

--- express-js/app.js
+++ express-js/app.js
@@ -2,12 +2,15 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First middleware');
+app.use('/', (req, res, next) => {
+  console.log('Run for every route');
   next();
 });
+app.use('/another-route', (req, res, next) => {
+  res.send('<h1>Another route page.</h1>');
+});
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
   res.send('<h1>Hello from express js!</h1>');
 });
 
