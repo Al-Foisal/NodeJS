a2a336b520792bac75473f8c811cf9e817733ae7
Md Hafiz Al Foisal <alfoisal940700@gmail.com>
2025-02-09 07:38:09 +0600

    form submission and parsing body through body-parser package

--- express-js/app.js
+++ express-js/app.js
@@ -1,13 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-  console.log('Run for every route');
-  next();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/add-product', (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+  );
 });
-app.use('/another-route', (req, res, next) => {
-  res.send('<h1>Another route page.</h1>');
+
+app.post('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
